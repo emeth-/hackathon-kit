084bd22bbe52a58ae494ef483285807a9c2171a4
emeth- <seanybob@gmail.com>
2017-04-26 20:12:29 -0500

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     url(r'^save_fish$', "api.views.save_fish"),
     url(r'^delete_fish$', "api.views.delete_fish"),
     url(r'^get_fish$', "api.views.get_fish"),
+    url(r'^load_widget$', "api.views.load_widget"),
     url(r'^$', "api.views.load_frontend")
 )
